261e4cabc25fb2b0b6cc0578868c8d9a2a632420
Gordon Dindi <Dindigordon1@gmail.com>
2023-03-24 13:16:46 +0200

    App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import FeedButton from "./components/buttons/feedButton";
 import React, { useState, useEffect } from "react";
 import SleepButton from "./components/buttons/sleepButton";
 import HugButton from "./components/buttons/hugButton";
-import Daybg from './components/images/Daybg.png';
+import Daybg from './components/images/Daybg';
 // import Nightbg from "./Nightbg.webp";
 
 
