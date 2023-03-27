1771edfb78512e08d5434a908e46c856d6680696
Nikshay <nikshaylalla8@gmail.com>
2023-03-27 13:40:43 +0200

    updated speechbubble

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
           setHealth={setHealth}
           setHappiness={setHappiness}
         />
-        {/* <SpeechBubble></SpeechBubble> */}
+        <SpeechBubble></SpeechBubble>
       </div>
     </div>
   );
