014ff8984d85c2c462432534532872aa9e1c87ba
Nikshay <nikshaylalla8@gmail.com>
2023-03-29 09:08:22 +0200

    updated

--- src/components/speechBubble.js
+++ src/components/speechBubble.js
@@ -3,29 +3,33 @@ import './speechBubble.css';
 
 function SpeechBubble() {
   const [inputText, setInputText] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
 
-  // const handleButtonClick = () => {
-  //   // do something with the inputText, e.g. submit it to a backend service
-  //   console.log('User input:', inputText);
-  // };
+  const handleButtonClick = () => {
+    // do something with the inputText, e.g. submit it to a backend service
+    console.log('User input:', inputText);
+    setSubmitted(true);
+  };
 
   return (
     <div>
-      <div className="speech-bubble">
-        <p> Hi my name is: </p>
-        <textarea 
-          className="speech-bubble__input"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <div className="speech-bubble__arrow"></div>
-        {/* <button onClick={handleButtonClick}>Submit</button> */}
-      </div>
-      {inputText && <div className="text-display">{inputText}</div>}
+      {!submitted && (
+        <div className="speech-bubble">
+          <p> Hi my name is: </p>
+          <textarea 
+            className="speech-bubble__input"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+          <div className="speech-bubble__arrow"></div>
+           <button onClick={handleButtonClick}>Submit</button> 
+        </div>
+      )}
+      {submitted && <div className="text-display">{inputText}</div>}
     </div>
   );
 }
